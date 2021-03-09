cab571ed8581bbebfa8bd3072bce63d215acb9ea
Raffy Traas <66647242+rtraas@users.noreply.github.com>
2021-03-08 20:33:35 -0600

    changed 'eSims' to 'mospy'

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
-import time; import eSims
+import time; import mospy
 
 start= time.perf_counter()
-eSims.doSim('thbaINFILE');
+mospy.doSim('thbaINFILE');
 end=time.perf_counter()
 elapsed=end-start
 print("elapsed time= {:.12f} seconds".format(elapsed))
