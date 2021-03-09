9d7582d0833d62a09e49581a06df9b3e2460928a
Raffy Traas <66647242+rtraas@users.noreply.github.com>
2021-03-08 20:36:19 -0600

    changed 'eSims' to 'mospy'

--- test_Inputs.py
+++ test_Inputs.py
@@ -3,12 +3,12 @@ import time
 testFiles=["/home/weiser/PycharmProjects/eSim2/mirror_orbits/eSims/eSims/test/t1INFILE.py","test/t2INFILE","test/t3INFILE","test/t4INFILE","test/t5INFILE","test/t6INFILE"]
 def _run():
     import sys
-    import eSims
+    import mospy
     print(sys.path)
 
     print(testFiles[0])
     start=time.perf_counter()
-    eSims.doSim(testFiles[0])
+    mospy.doSim(testFiles[0])
     end=time.perf_counter()
     elapsed=end-start
     print("elapsed time= {:.12f} seconds".format(elapsed))
